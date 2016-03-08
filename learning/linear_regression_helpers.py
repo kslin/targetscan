aa848ba1dfa20865cbd6c6824c2f99db5179e183
Kathy Lin <klin14@tak.wi.mit.edu>
2016-03-08 17:53:42 -0500

    started multisite model

--- learning/linear_regression_helpers.py
+++ learning/linear_regression_helpers.py
@@ -1,5 +1,7 @@
 import copy
 import random
+import time
+import sys
 
 import numpy as np
 import pandas as pd
@@ -16,6 +18,12 @@ GENE_FILE = '../../targetscan_files/Gene_info.txt'
 SEED_FILE = '../../seed_dict.csv'
 TS7_human_file = '../../Summary_Counts_Human7.txt'
 
+cols = ['Threep score','Local AU score','Min dist score','TA', 'SPS',
+        'UTR length score','Off6m score','SA',
+        'siRNA 1A', 'siRNA 1C', 'siRNA 1G', 'siRNA 8A',
+        'siRNA 8C', 'siRNA 8G', 'site 8A', 'site 8C', 'site 8G', 
+        'PCT','ORF length','ORF 8mers']
+
 
 def get_r_squared(x,y):
     slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
@@ -28,62 +36,69 @@ def get_logfc(df,gene,seed):
         return float('NaN')
     return float(df.loc[gene][seed])
 
-def get_data():
-    GENE_INFO = pd.read_csv(GENE_FILE,sep='\t').drop(['Gene description','Species ID'],1)
-    GENE_INFO = GENE_INFO.groupby('Gene symbol').agg(lambda x:tuple(x))
-    GENE_INFO.loc[:,'Isoform ratio'] = [float(max(x))/np.nansum(x) for x in GENE_INFO['3P-seq tags + 5']]
-    GENE_INFO.loc[:,'Transcript ID'] = [x[y.index(1)] for (x,y) in zip(GENE_INFO['Transcript ID'],GENE_INFO['Representative transcript?'])]
-    GENE_INFO = GENE_INFO[['Transcript ID','Isoform ratio']]
+def get_num_coop(locs,types):
+    upper = 46
+    lower = 13
+    locs = [l for (l,t) in zip(locs,types) if t != '6mer']
+    if len(locs) < 2:
+        return 0
+    num_coop = 0
+    for i in range(len(locs)-1):
+        diff = locs[i+1] - locs[i]
+        if (diff < 46) & (diff > 13):
+            num_coop += 1
+    return num_coop
 
-    SEED_INFO = pd.read_csv(SEED_FILE,sep='\t')
-    SEED_DICT = {}
-    for row in SEED_INFO.iterrows():
-        SEED_DICT[row[1]['col']] = row[1]['seed']
+def organize0(data,site_specific):
+        subdf = copy.deepcopy(data)
+        subdf['single site score'] = site_specific.predict(data,cols)
+        subdf = subdf.groupby('ID').agg({'single site score': lambda x: tuple(x),
+                                                       'logFC': lambda x: tuple(x)[0],
+                                                       'Site type': lambda x: tuple(x),
+                                                       'Site end': lambda x: tuple(x),
+                                                       'Num sites': lambda x: tuple(x)[0],
+                                                       'UTR length score': np.nanmean,
+                                                       'special id':lambda x: tuple(x)})
+        subdf['Num coop'] = [get_num_coop(locs,sites) for (locs,sites) in zip(subdf['Site end'],subdf['Site type'])]
+        subdf['Num sites score'] = np.divide(subdf['Num sites'],subdf['UTR length score'])
+        subdf['Num coop score'] = np.divide(subdf['Num coop'],subdf['UTR length score'])
+        subdf['Single site score'] = [sum(x) for x in subdf['single site score']]
+        return subdf
 
-    logFCs1 = pd.read_csv(LOGFC_FILE1)
-    logFCs1['Transcript ID'] = list(GENE_INFO.loc[logFCs1['Gene symbol']]['Transcript ID'])
-    logFCs1['Isoform ratio1'] = list(GENE_INFO.loc[logFCs1['Gene symbol']]['Isoform ratio'])
-    logFCs1 = logFCs1.drop(['Used in training','RefSeq ID','Gene symbol'],1).dropna(subset=['Transcript ID'])
-    logFCs1 = logFCs1.drop_duplicates(subset=['Transcript ID']).set_index('Transcript ID')
-    logFCs1.columns = [SEED_DICT[x] if x in SEED_DICT else x for x in logFCs1.columns]
+def organize(data,site_specific,old_subdf):
+        subdf = copy.deepcopy(data)
+        subdf['single site score'] = site_specific.predict(data,cols)
+        subdf = subdf[['ID','single site score']]
+        subdf = subdf.groupby('ID').agg(lambda x: tuple(x))
 
-    logFCs2 = pd.read_csv(LOGFC_FILE2)
-    logFCs2['Transcript ID'] = list(GENE_INFO.loc[logFCs2['Gene symbol']]['Transcript ID'])
-    logFCs2['Isoform ratio2'] = list(GENE_INFO.loc[logFCs2['Gene symbol']]['Isoform ratio'])
-    logFCs2 = logFCs2.drop(['RefSeq ID','Gene symbol'],1).dropna(subset=['Transcript ID'])
-    logFCs2 = logFCs2.drop_duplicates(subset=['Transcript ID']).set_index('Transcript ID')
-    logFCs2.columns = [SEED_DICT[x] if x in SEED_DICT else x for x in logFCs2.columns]
+        old_len = len(subdf)
+        subdf = pd.concat([subdf,old_subdf.drop('single site score',1)],axis=1,join='inner')
 
-    logFCs = pd.concat([logFCs1,logFCs2],axis=1,join='outer')
+        assert len(subdf) == old_len, '{},{}'.format(len(subdf),old_len)
+
+        subdf['Single site score'] = [sum(x) for x in subdf['single site score']]
+
+        return subdf
+
+def prepare_data_for_site_specific(data, original):
+    t0 = time.time()
+    adjusted_logFC = list(np.subtract(data['logFC'],data['multiscore']))
+    zipped = zip(data['Single site score'],data['single site score'],adjusted_logFC)
+    new_logfcs = [[(x/site_score)*logfc if (site_score != 0) else (logfc/len(site_scores)) for x in site_scores] for (site_score,site_scores,logfc) in zipped]
+    # t0 = time.time()
+    ids = list(sum(list(data['special id']), ()))
+    # print 'woah4 {}'.format(time.time()-t0)
+    # t0 = time.time()
+    new_logfcs = np.array(sum(new_logfcs,[]))
+    # print 'woah5 {}'.format(time.time()-t0)
+    # t0 = time.time()
+
+    return_df = copy.deepcopy(original)
+    return_df['logFC'] = new_logfcs[np.argsort(ids)]
+
+    return return_df
 
 
-    features = pd.read_csv(FEATURE_FILE, sep='\t')
-
-    features['logFC'] = [get_logfc(logFCs,gene,seed) for (gene,seed) in zip(features['Gene ID'],features['Seed'])]
-    features = features.dropna()
-
-    isoform_ratios = [y if np.isnan(x) else x for (x,y) in zip(logFCs['Isoform ratio1'],logFCs['Isoform ratio2'])]
-    logFCs['Isoform ratio'] = isoform_ratios
-
-    features['Isoform ratio'] = list(logFCs.loc[features['Gene ID']]['Isoform ratio'])
-    features['Num sites'] = [1]*len(features)
-    num_sites = features.groupby(['Gene ID','Seed']).agg({'Num sites':np.nansum})
-    features['Num sites'] = list(num_sites.loc[zip(features['Gene ID'],features['Seed'])]['Num sites'])
-
-    return features
-
-def get_ts7():
-    targetscan7 = pd.read_csv(TS7_human_file,sep='\t',usecols=['Transcript ID','Gene Symbol','miRNA family',
-                                                                                                    'Total num conserved sites',
-                                                                                                    'Cumulative weighted context++ score'])
-    targetscan7 = targetscan7[targetscan7['Transcript ID'].isin(GENE_INFO['Transcript ID'])]
-    targetscan7 = targetscan7[targetscan7['miRNA family'].isin(all_seeds)]
-    targetscan7 = targetscan7.rename(columns={'Gene Symbol':'Gene symbol'})
-    targetscan7.loc[:,'logFC'] = [get_logfc())(targetscan7['Gene symbol'],targetscan7['miRNA family'])
-    targetscan7 = targetscan7.dropna(subset=['logFC'])
-    targetscan7 = targetscan7.set_index('miRNA family')
-    
-    return targetscan7
 
 #### Training methods #####
 
@@ -141,43 +156,6 @@ class SiteSpecific(object):
         return coefs,intercepts
 
 
-
-
-
-def organize0(data,site_specific):
-        subdf = copy.deepcopy(data)
-        subdf['special id'] = range(len(data))
-        subdf['single site score'] = site_specific.predict(data,cols)
-        subdf = subdf.groupby('ID').agg({'single site score': lambda x: tuple(x),
-                                                       'logFC': lambda x: tuple(x)[0],
-                                                       'Site type': lambda x: tuple(x),
-                                                       'Site end': lambda x: tuple(x),
-                                                       'Num sites': lambda x: tuple(x)[0],
-                                                       'UTR length score': np.nanmean,
-                                                       'special id':lambda x: tuple(x)})
-        subdf['Num coop'] = [get_num_coop(locs,sites) for (locs,sites) in zip(subdf['Site end'],subdf['Site type'])]
-        subdf['Num sites score'] = np.divide(subdf['Num sites'],subdf['UTR length score'])
-        subdf['Num coop score'] = np.divide(subdf['Num coop'],subdf['UTR length score'])
-        subdf['Single site score'] = [sum(x) for x in subdf['single site score']]
-        return subdf
-
-def organize(data,site_specific):
-        subdf = copy.deepcopy(data)
-        subdf['special id'] = range(len(data))
-        subdf['single site score'] = site_specific.predict(data,cols)
-        subdf = subdf.groupby(['Gene ID','Seed']).agg({'single site score': lambda x: tuple(x),
-                                                       'logFC': np.nansum,
-                                                       'Site type': lambda x: tuple(x),
-                                                       'Site end': lambda x: tuple(x),
-                                                       'Num sites': lambda x: tuple(x)[0],
-                                                       'UTR length score': np.nanmean,
-                                                       'special id':lambda x: tuple(x)})
-        subdf['Num coop'] = [get_num_coop(locs,sites) for (locs,sites) in zip(subdf['Site end'],subdf['Site type'])]
-        subdf['Num sites score'] = np.divide(subdf['Num sites'],subdf['UTR length score'])
-        subdf['Num coop score'] = np.divide(subdf['Num coop'],subdf['UTR length score'])
-        subdf['Single site score'] = [sum(x) for x in subdf['single site score']]
-        return subdf
-
 class MultiSite(object):
     
     def __init__(self, normalize=False):
@@ -201,18 +179,6 @@ class MultiSite(object):
     def get_coefs(self):
         return self.ols_model.coef_, self.ols_model.intercept_
 
-def prepare_data_for_site_specific(data, original):
-    data = copy.deepcopy(data)
-    data['adjusted logFC'] = np.subtract(data['logFC'],data['multiscore'])
-    new_logfcs = [[(x/site_score)*logfc if (site_score != 0) else (logfc/len(site_scores)) for x in site_scores] for (site_score,site_scores,logfc) in zip(data['Single site score'],
-                                                                                                                                 data['single site score'],
-                                                                                                                                 data['adjusted logFC'])]
-    ids = reduce(lambda x,y: x+y, list(data['special id']))
-    new_logfcs = np.array(reduce(lambda x,y: x+y, new_logfcs))
-    return_df = copy.deepcopy(original)
-    return_df['logFC'] = new_logfcs[np.argsort(ids)]
-
-    return return_df
 
 
 
